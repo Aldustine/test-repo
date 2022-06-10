f072e855f6a8961975d84216bc3bff5d83120e7c
Maksym Melnyk <maksym_m@optimove.com>
2022-06-10 11:57:10 +0300

    fhlalsdjfnadf

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
             Console.WriteLine("goodbay world");
             Console.WriteLine("Hello hell");
-
+            Console.WriteLine("Hello hell");
             Console.WriteLine("Hello hell");
             Console.WriteLine("Hello hell");
             Console.WriteLine("Hello hell");
